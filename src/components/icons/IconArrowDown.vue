a9140f5a7395a8f7b57a97d135a3a1c7249308cd
Tatyana <syrtsova.tanya@gmail.com>
2021-08-06 16:32:09 +0700

    Add base select

---
+++ src/components/icons/IconArrowDown.vue
@@ -0,0 +1,11 @@
+<template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="8" height="8" fill="none">
+    <path stroke="currentColor" d="M7.49 3.24L4.24 6.5 1 3.24" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'IconArrowDown',
+};
+</script>
