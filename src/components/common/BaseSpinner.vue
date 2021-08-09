79ab482c60f5452ad5eca3641bbe2e87fd6870e9
Tatyana <syrtsova.tanya@gmail.com>
2021-08-09 13:46:13 +0700

    Add base loader

---
+++ src/components/common/BaseSpinner.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="base-spinner"></div>
+</template>
+
+<script>
+export default {
+  name: 'BaseSpinner',
+};
+</script>
+
+<style lang="scss" scoped>
+.base-spinner {
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+
+  border-top: 3px solid rgba($--green, 0.2);
+  border-right: 3px solid rgba($--green, 0.2);
+  border-bottom: 3px solid rgba($--green, 0.2);
+  border-left: 3px solid $--green;
+  animation: rotate 0.7s infinite linear;
+}
+
+@keyframes rotate {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
